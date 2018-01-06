be85934c1ec30eb61e6a2437c9635e13689c1221
manvika123 <manvikajaiswal1995@gmail.com>
2018-01-06 21:20:00 +0530

    first commit

---
+++ src/Temperature.java
@@ -0,0 +1,10 @@
+public class Temperature {
+    public static void main(String[] args) {
+
+        double c= 10.5;
+        double f;
+
+        f= (((9.0/5.0)*c)+32.0);
+        System.out.println("the temperature in fahrenheit is" +f);
+    }
+}
